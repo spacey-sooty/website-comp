b7caa8deace3d2f61e0f15e4f9c82009aa160e14
Isaac <skankhunt49@outlook.com.au>
2023-05-08 13:54:59 +0800

    lol

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <html>   
       <body>
         <h1 id="font1"> CJ is good teacher </h1>
+        <p id="font2"> This is a nextjs project. Don't you think im so smort? </p>
       </body>
     </html>
   
