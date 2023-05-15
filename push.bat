876e484a81e8c846e7d535484195e86a57e34725
Isaac <skankhunt49@outlook.com.au>
2023-05-15 23:52:41 +0800

    test2

--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
-echo Batch Script to take input.
+echo Git Add, Commit and Push.
 set /p input= Commit message?
-echo Input is: %input%
+@REM echo Input is: %input%
 git add .
 git commit -m %input%
 git push
